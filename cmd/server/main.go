e2b82353ac77ea9791dc29c9a37854344bd798b9
Kevin Wang <kevmo314@gmail.com>
2021-12-01 13:22:58 -0500

    seq logging

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -159,11 +159,16 @@ func NewRTPSender(rtc *sdk.RTC, tid string, codec *packets.Codec, rtpIn rtpio.RT
 	if _, err := rtc.Publish(track); err != nil {
 		return err
 	}
+	prevSeq := uint16(0)
 	for {
 		p := &rtp.Packet{}
 		if _, err := rtpIn.ReadRTP(p); err != nil {
 			return nil
 		}
+		if p.SequenceNumber != prevSeq + 1 {
+			log.Warn().Uint16("PrevSeq", prevSeq).Uint16("CurrSeq", p.SequenceNumber).Msg("missing packet")
+		}
+		prevSeq = p.SequenceNumber
 		if err := track.WriteRTP(p); err != nil {
 			log.Warn().Err(err).Msg("failed to write sample")
 		}
